fd6dab1d7ea56791f162585e4578e099654acf9d
David van Enckevort <david.van.enckevort@umcg.nl>
2018-01-02 16:06:12 +0100

    Implemented content panel

--- views/lifecycle.js
+++ views/lifecycle.js
@@ -21,7 +21,132 @@ function loadFromMolgenisEntity(event, data) {
 	data.result = request.then(convertMolgenisResponse).promise();
 }
 
-function showDetails(event, data) {
+function createTable(colums, rows) {
+	var html = '<table class="table table-striped table-condensed table-bordered molgenis-table">'
+	html += '<tr><thead>'
+	for (let column of colums) {
+		html += '<th>' + column + '</th>'
+	}
+	html += '</thead></tr>'	
+	html += rows()
+	html += '</table>'
+	return html
+}
+
+function createRow(columns, data, metadata) {
+	var html = '<tr>';
+	for (let column of columns) {
+		let variable = data[column];
+		if (variable === undefined) {
+			html += '<td></td>';
+			continue;
+		}
+		let fieldType = metadata[column].fieldType;
+		switch (fieldType) {
+		case 'TEXT':
+		case 'SCRIPT':
+			html += '<td><pre>' + variable + '</pre></td>';
+			break;
+		case 'CATEGORICAL':
+		case 'XREF':
+			{
+				let labelAttribute = metadata[column].refEntity.labelAttribute;
+				html += '<td>' + variable[labelAttribute] + '</td>';				
+			}
+			break;			
+		case 'CATEGORICAL_MREF':
+		case 'ONE_TO_MANY':
+		case 'MREF':
+			{
+				let labelAttribute = metadata[column].refEntity.labelAttribute;
+				html += '<td>'
+				for (let item of variable) {
+					html += item[labelAttribute] + '<br>';
+				}
+				html += '</td>'
+			}
+			break;
+		default:
+			html += '<td>' + variable + '</td>';
+		}
+	}
+	html += '</tr>';
+	return html;
+}
+
+function extractMetadata(metadata) {
+	let dict = {};
+	for (let attribute of metadata) {
+		dict[attribute.name] = attribute;
+	}
+	return dict
+}
+
+function createCoreVariablesTable(data) {
+	let columns = ['variable', 'label', 'values', 'datatype', 'comments'];
+	let metadata = extractMetadata(data.meta.attributes);
+	let labels = columns.map(function(column) { return metadata[column].label; });
+	var html = '<div id="content-panel">';
+	html += createTable(labels, function() {
+		var rows;
+		for (let item of data.items) {
+			rows += createRow(columns, item, metadata);
+		}
+		return rows;
+	});
+	html += '</div>';
+	return html;
+}
+
+function createHarmonizationRow(item, cohorts, metadata) {
+	var html = '<tr><td>' + item.variable + '</td>';
+	let labelAttribute = metadata['harmonizations'].refEntity.labelAttribute;
+	let harmonizedCohorts = item.harmonizations.map(function(value) { return value[labelAttribute]; });
+	for (let cohort of cohorts) {
+		var harmonizationStatus;
+		if (harmonizedCohorts.indexOf(cohort) !== -1) {
+			harmonizationStatus = '<span color="green" fontsize="large">✓</span>';
+		} else {
+			harmonizationStatus = '<span color="red" fontsize="large">𐄂</span>';
+		}
+		html += '<td>' + harmonizationStatus + '</td>'
+	}
+	html += '</tr>'
+	return html;
+}
+
+function createHarmonizationsTable(cohorts, data) {
+	let labels = ['Variable'].concat(cohorts);
+	let metadata = extractMetadata(data.meta.attributes);
+	var html = createTable(labels, function() {
+		var rows;
+		for (let item of data.items) {
+			rows += createHarmonizationRow(item, cohorts, metadata);
+		}
+		return rows;
+	});
+}
+
+function createContentPanel(data) {
+	var html = createCoreVariablesTable(data);
+	var request = $.getJSON('/api/v2/LifeCycle_Cohorts');
+	request.then(function(response) {
+		let labelAttribute = response.meta.labelAttribute;
+		let cohorts = response.items.map(function(item) { return item[labelAttribute]});
+		return html + createHarmonizationsTable(cohorts, data);	
+	}).done(function(content) {
+		$("#content-panel").replaceWith(content);		
+	});
+}
+
+function loadContentPanel(event, data) {
+	var variables = data.node.data.variables;
+	if (variables === undefined || variables.length === 0) {
+		return;
+	}
+	var list = variables.map(function(v) { return v.id }).join();
+	var request = $.getJSON('/api/v2/LifeCycle_CoreVariables?q=id=in=(' + list +')');
+	request.done(createContentPanel);
 }
 
 function init() {
@@ -33,7 +158,7 @@ function init() {
 			key: 'root'
 		}],
 		lazyLoad: loadFromMolgenisEntity,
-		activate: showDetails,
+		activate: loadContentPanel,
 		icon: true
 	});
 }
